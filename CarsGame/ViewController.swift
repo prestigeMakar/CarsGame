7090b3b43bb0bcde9bd03942bd8b09f122783fb4
Artem Makarenko <prestigemakar@gmail.com>
2022-06-30 15:51:37 +0300

    Initial Commit

---
+++ CarsGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarsGame
+//
+//  Created by Артем Макаренко on 30.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
